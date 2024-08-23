baf7c0cc23f6369b88d5fadb5f69a225a2a56839
Reggie <reggie.senal@gmail.com>
2024-08-23 15:16:53 +0800

    add test 12

--- src/com/exam/test12/Solution.java
+++ src/com/exam/test12/Solution.java
@@ -1,16 +1,33 @@
-package com.exam.test11;
+package com.exam.test12;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+
+class Result {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day);
+        int dayNumber = cal.get(Calendar.DAY_OF_WEEK);
+        String dayName = new DateFormatSymbols().getWeekdays()[dayNumber];
+        return dayName.toUpperCase();
+    }
+}
 
 public class Solution {
-    public static void main(String[] argh) {
-        Scanner scan = new Scanner(System.in);
-        try {
-            int value = scan.nextInt();
-            String s = String.valueOf(value);
-            System.out.println("Good job");
-        } catch (Exception e) {
-            System.out.println("Wrong answer");
-        }
+    public static void main(String[] args) throws IOException {
+        String v = Result.findDay(8, 5, 2015);
+        System.out.println(v);
     }
 }
