7f5bfe009bae6f18f09f9816a22cfacfdc1243e8
Reggie <reggie.senal@gmail.com>
2024-08-23 16:33:06 +0800

    add test 13

---
+++ src/com/exam/test13/Solution.java
@@ -0,0 +1,16 @@
+package com.exam.test11;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] argh) {
+        Scanner scan = new Scanner(System.in);
+        try {
+            int value = scan.nextInt();
+            String s = String.valueOf(value);
+            System.out.println("Good job");
+        } catch (Exception e) {
+            System.out.println("Wrong answer");
+        }
+    }
+}
