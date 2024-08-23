b18be9eb1ff1d8ccbc7ccdf12496e9e49f41baf1
Reggie <reggie.senal@gmail.com>
2024-08-23 16:33:10 +0800

    add test 13

--- src/com/exam/test13/Solution.java
+++ src/com/exam/test13/Solution.java
@@ -1,16 +1,29 @@
-package com.exam.test11;
+package com.exam.test13;
 
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] argh) {
-        Scanner scan = new Scanner(System.in);
-        try {
-            int value = scan.nextInt();
-            String s = String.valueOf(value);
-            System.out.println("Good job");
-        } catch (Exception e) {
-            System.out.println("Wrong answer");
-        }
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = getCurrencyValue(payment, Locale.US);
+        String india = getCurrencyValue(payment, new Locale("en", "IN"));
+        String china = getCurrencyValue(payment, Locale.CHINA);
+        String france = getCurrencyValue(payment, Locale.FRANCE);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+
+    private static String getCurrencyValue(double value, Locale locale) {
+        NumberFormat nF = NumberFormat.getCurrencyInstance(locale);
+        return nF.format(value);
     }
 }
