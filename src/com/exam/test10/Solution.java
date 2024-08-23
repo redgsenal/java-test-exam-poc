903392806183b9afbe1e7426cfbdb40d7bcdba2c
Reggie <reggie.senal@gmail.com>
2024-08-23 14:06:25 +0800

    add test 10 and test 9

--- src/com/exam/test10/Solution.java
+++ src/com/exam/test10/Solution.java
@@ -1,15 +1,24 @@
-package com.exam.test9;
+package com.exam.test10;
 
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] argh) {
-        Scanner sc = new Scanner(System.in);
-        int i = 0;
-        while (sc.hasNext()) {
-            String line = sc.nextLine();
-            System.out.printf("%d %s\n", ++i, line);
+        Scanner scan = new Scanner(System.in);
+        int b = scan.nextInt();
+        int h = scan.nextInt();
+        try {
+            int area = computeParallelogramArea(b, h);
+            System.out.println(area);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        sc.close();
+    }
+
+    private static int computeParallelogramArea(int b, int h) throws Exception {
+        if (b <= 0 || h <= 0) {
+            throw new Exception("java.lang.Exception: Breadth and height must be positive");
+        }
+        return b * h;
     }
 }
--- src/com/exam/test9/Solution.java
+++ src/com/exam/test9/Solution.java
@@ -1,42 +1,15 @@
-package com.exam.test8;
+package com.exam.test9;
 
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] argh) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            try {
-                long x = sc.nextLong();
-                System.out.println(x + " can be fitted in:");
-                if (isByte(x)) System.out.println("* byte");
-                if (isShort(x)) System.out.println("* short");
-                if (isInt(x)) System.out.println("* int");
-                if (isLong(x)) System.out.println("* long");
-            } catch (Exception e) {
-                System.out.println(sc.next() + " can't be fitted anywhere.");
-            }
+        int i = 0;
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            System.out.printf("%d %s\n", ++i, line);
         }
-    }
-
-    private static boolean isBetweenRange(long min, long max, long x) {
-        return (x >= min && x <= max);
-    }
-
-    private static boolean isByte(long x) {
-        return isBetweenRange(-128, 127, x);
-    }
-
-    private static boolean isShort(long x) {
-        return isBetweenRange(-32768, 32767, x);
-    }
-
-    private static boolean isInt(long x) {
-        return isBetweenRange(-2147483648, 2147483647, x);
-    }
-
-    private static boolean isLong(long x) {
-        return isBetweenRange(-9223372036854775808L, 9223372036854775807L, x);
+        sc.close();
     }
 }
