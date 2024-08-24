62ee1576cd0926b21787c3e1dbbea6b153c16147
Reggie <reggie.senal@gmail.com>
2024-08-24 19:01:25 +0800

    add test19

---
+++ src/com/exam/test19/Solution.java
@@ -0,0 +1,33 @@
+package com.exam.test19;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //Scanner scan = new Scanner(System.in);
+        //String s = scan.nextLine();
+        displayStringToken("He is a very very good boy, isn't he?");
+        //scan.close();
+    }
+
+    private static void displayStringToken(String s) {
+        if (s == null || s.isEmpty()) {
+            return;
+        }
+        List<String> tokens = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[A-Za-z]+");
+        Matcher matcher = pattern.matcher(s);
+        int count = 0;
+        while (matcher.find()) {
+            String value = matcher.group();
+            tokens.add(value);
+        }
+        System.out.println(tokens.size());
+        tokens.forEach(System.out::println);
+    }
+}
