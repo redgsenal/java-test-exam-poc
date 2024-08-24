076cd388db41196aa319b78572d16b24aa87393e
Reggie <reggie.senal@gmail.com>
2024-08-24 10:55:27 +0800

    update test15

---
+++ src/com/exam/test15/Solution.java
@@ -0,0 +1,15 @@
+package com.exam.test15;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String result = S.substring(start, end);
+        System.out.println(result);
+    }
+}
