4d7c307ff09ff995c3c722e247ebccb71815e0bf
Reggie <reggie.senal@gmail.com>
2024-08-30 09:40:17 +0800

    update for test20

---
+++ src/com/exam/test20/Solution.java
@@ -0,0 +1,23 @@
+package com.exam.test20;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+
+class MyRegex {
+
+    public String pattern = "\\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
+
+    public static void MyRegex() {
+    }
+
+}
