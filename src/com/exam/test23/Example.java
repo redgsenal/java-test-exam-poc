ea36c0c9fa693551ee44431dcede8cc9511ea411
Reggie <reggie.senal@gmail.com>
2024-09-03 13:40:33 +0800

    work on test23

--- src/com/exam/test23/Example.java
+++ src/com/exam/test23/Example.java
@@ -1,23 +1,32 @@
 package com.exam.test23;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-public class Solution {
+public class Example {
 
+    // private static String TAG_PATTERN = "<\\s*([a-zA-Z0-9]+)([^>]*)>(.*?)<\\/\\1\\s*>";
     private static final String TAG_PATTERN = "<(\\w+)([^<]*?)>(?:[^<]*<\\1[^<]*>)*(.*?)<\\/\\1>";
-    private static final Pattern PATTERN = Pattern.compile(TAG_PATTERN, Pattern.UNICODE_CASE);
-
-    public static void main(String[] args) {
+    //private static String TAG_PATTERN = "<(\\w+)([^<]*?)>(.*?)<\\/\\1>";
+    // private static final Pattern PATTERN = Pattern.compile(TAG_PATTERN, Pattern.UNICODE_CASE);
+    private static final Pattern PATTERN = Pattern.compile(TAG_PATTERN, Pattern.LITERAL);
+    /*public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         int testCases = Integer.parseInt(in.nextLine());
-        while (testCases > 0) {
+        while(testCases>0){
             String line = in.nextLine();
             displayTagContents(line);
             testCases--;
         }
+    }*/
+
+    public static void main(String[] args) {
+        //displayTagContents("<a><a>Sanjay has no watch</a></a><par>So wait for a while</par>");
+        // displayTagContents("<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>");
+        //displayTagContents("<h1>Hello</h1><h1>Sanjay has no watch</h1><par>So wait for a while</par>");
+        // displayTagContents("<body><p><h1>Sanjay has no watch</h1></p><par>So wait for a while</par></body>");
+        displayTagContents("<Amee>safat codes like a ninja</amee>");
     }
 
     private static boolean hasTags(String line) throws Exception {
